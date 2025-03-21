277c2b3f27320725a771d00cd4944234c682f647
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2025-03-21 16:22:33 -0400

    appendonly flag

--- sources_demo.py
+++ sources_demo.py
@@ -7,7 +7,7 @@ site.login()
 
 
 
-def put_collection_pages(id:int) -> void:
+def put_collection_pages(id:int) -> None:
 	"""
 	This method creates (overwriting) all of the pages associated with each source in a collection:
 		- the Base Page
@@ -19,6 +19,10 @@ def put_collection_pages(id:int) -> void:
 
 	These are separated out into distinct pages so that other bot processes down the line can edit one component without needing any
 	weird and potentially destructive edits to the base page. 
+
+	Edits to the Infobox, Collections, and Feeds, will all be things that can happen destructively within their own domain
+	and the Changelog can be setup to be an 'only append' situation, but the BasePage needs to not be touched by a bot ever,
+	so that human-written changes can be preserved
 	"""	
 	coll: Collection = Collection.from_id(us_national)
 
@@ -29,15 +33,20 @@ def put_collection_pages(id:int) -> void:
 		put_page_text(t.render_infobox)
 		put_page_text(t.render_collections)
 
-def put_page_text(render_method):
+def put_page_text(render_method, append_only=False):
 	page_title, page_text = render_method()
 	page = pywikibot.Page(site, page_title)
 	if page.exists():
 		page.text = page_text 
 		page.save(summary="Updating source page via bot")
 	else:
-		page.text = page_text 
+		if append_only:
+			page_text += page_text
+		else:
+			page.text = page_text 
 		page.save(summary = "Creating new source page via bot")
+
+
 
 if __name__ == "__main__":
 	us_national = 34412234
