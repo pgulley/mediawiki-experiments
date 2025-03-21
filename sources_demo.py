7aae2e8f50b93b65609b331a24de17dca2f2ad2e
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2025-03-21 16:02:10 -0400

    fine I'll try for good python hygene ok

--- sources_demo.py
+++ sources_demo.py
@@ -5,15 +5,17 @@ from directory.directory import Collection
 site = pywikibot.Site()
 site.login()
 
-us_national = 34412234
-coll: Collection = Collection.from_id(us_national)
 
-for t in coll.iter_sources():
-	print(t.wiki_name())
 
-	put_page_text(t.render_base_page)
-	put_page_text(t.render_infobox)
-	put_page_text(t.render_collections)
+def put_collection_pages(id:int) -> void:
+	coll: Collection = Collection.from_id(us_national)
+
+	for t in coll.iter_sources():
+		print(t)
+
+		put_page_text(t.render_base_page)
+		put_page_text(t.render_infobox)
+		put_page_text(t.render_collections)
 
 def put_page_text(render_method):
 	page_title, page_text = render_method()
@@ -24,3 +26,7 @@ def put_page_text(render_method):
 	else:
 		page.text = page_text 
 		page.save(summary = "Creating new source page via bot")
+
+if __name__ == "__main__":
+	us_national = 34412234
+	put_collection_pages(us_national)
